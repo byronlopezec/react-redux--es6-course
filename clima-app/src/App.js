d936bfbdfb0a16e06629d12b67a09858c0cd109e
Buhobit <byron.lopez.begins@gmail.com>
2018-11-04 12:42:19 -0500

    71. React Flexbox Autosize, Alignment, Distribution

--- clima-app/src/App.js
+++ clima-app/src/App.js
@@ -16,14 +16,29 @@ class App extends Component {
     return (
       <Grid fluid>
         <Row>
-          <Col xs >
+          Grid System, xs, sm, md, lg
+        </Row>
+        <Row>
+          <h5>Las columnas se muestran en la misma fila mientras sumen 12</h5>
+        </Row>
+        {/* <Row center="xs"> Alinear al centro */}
+        {/* <Row around="xs"> Deja espacios entre columnas */}
+        {/* <Row between="xs"> a diferencia de around ocupa los filos y deja el mismo espacio entre columnas como en around */}
+        <Row around="xs">
+          {/* <Col xsOffset={6} xs > las primeros 6 columnas estan libres 
+          y se dibujan las 4 columnas restantes en el resto de la pantalla */}
+          <Col xs={2} >
             <div className="red"> </div>
           </Col>
-          <Col xs >
+          {/*<Col xs > Autosize al tamanio xtreme small */}
+          <Col xs={2} >
             <div className="blue"> </div>
           </Col>
-          <Col xs >
+          <Col xs={2} >
             <div className="green"></div>
+          </Col>
+          <Col xs={2} >
+            <div className="yellow"></div>
           </Col>
         </Row>
       </Grid>
