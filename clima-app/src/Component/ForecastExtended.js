d402a4c3f626e9b5399b2cd0941a1a4b8fe601b3
Buhobit <byron.lopez.begins@gmail.com>
2018-11-04 23:08:55 -0500

    89. Utilización de array de componentes

--- clima-app/src/Component/ForecastExtended.js
+++ clima-app/src/Component/ForecastExtended.js
@@ -2,13 +2,28 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types'
 import ForecastItem from './ForecastItem/ForecastItem';
 
+const days = [
+    'Lunes',
+    'Martes',
+    'Miercoles',
+    'Jueves',
+    'Viernes',
+];
+
 class ForecastExtended extends Component {
+    
+    renderForecastItemDays(days) {
+
+        return (
+            days.map(day => <ForecastItem key={day} weekDay={day} />)
+        );
+    }
     render() {
         const city = this.props.city;
         return (
             <div>
                 <h2 className='forecast-title'>Prognóstico extendido {city}</h2>
-                <ForecastItem />
+                {this.renderForecastItemDays(days)}
             </div>
         );
     }
