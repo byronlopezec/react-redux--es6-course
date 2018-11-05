4239ab42f7227f2ce56eeda4fda599aaf58a58a5
Buhobit <byron.lopez.begins@gmail.com>
2018-11-04 22:19:32 -0500

    88. Creación componente ForecastItem

--- clima-app/src/Component/ForecastExtended.js
+++ clima-app/src/Component/ForecastExtended.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types'
+import ForecastItem from './ForecastItem/ForecastItem';
 
 class ForecastExtended extends Component {
     render() {
         const city = this.props.city;
-        return (<div className='forecast-title'>Prognóstico extendido {city}</div>);
+        return (
+            <div>
+                <h2 className='forecast-title'>Prognóstico extendido {city}</h2>
+                <ForecastItem />
+            </div>
+        );
     }
 }
 
