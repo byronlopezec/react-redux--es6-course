38f9debe8cff2f174cbd482afa20116b5d0ea39c
Buhobit <byron.lopez.begins@gmail.com>
2018-11-17 20:54:51 -0500

    101. Mejora visual

--- clima-app/src/Component/ForecastItem/ForecastItem.js
+++ clima-app/src/Component/ForecastItem/ForecastItem.js
@@ -6,7 +6,7 @@ import WeatherData from '../WeatherLocation/WeatherData'
 const ForecastItem = ({ weekDay, hour, data }) => {
     return (
         <div>
-            <div>{weekDay} Hora: {hour} hs </div>
+            <h2>{weekDay} - {hour} hs </h2>
             <WeatherData data={data} />
         </div>
     );
