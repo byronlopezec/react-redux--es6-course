1c35b969879c2e465064b7b3314867b6c7c24a12
Buhobit <byron.lopez.begins@gmail.com>
2018-11-18 23:26:28 -0500

    122. Creación de container LocationListContainer

--- clima-app/src/App.js
+++ clima-app/src/App.js
@@ -1,12 +1,10 @@
 import { Grid, Row, Col } from 'react-flexbox-grid';
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 import './App.css';
-import LocationList from './Component/LocationList';
+
 import { AppBar, Toolbar, Typography, Paper } from '@material-ui/core';
 import ForecastExtended from './Component/ForecastExtended';
-import { setCityActionCreator } from './Actions';
-import PropTypes from 'prop-types';
+import LocationListContainer from './Containers/LocationListContainer';
 
 const countries = [
   "Quito,ec",
@@ -16,21 +14,13 @@ const countries = [
 ]
 
 class App extends Component {
+
   constructor() {
     super();//antes de usar cualquier this, esto llama al constructor de component he inicializa state.
     this.state = {
       city: null
     };
   }
-
-  handleSelectedLocation = (city) => {
-    this.setState({ city: city });
-    console.log(`handleSelectedLocation ${city}`)
-
-    //en vez de enviar directamente una action, se envia un actionCreator que devuelve 
-    this.props.setCity(city);
-  }
-
   render() {
     const { city } = this.state;
     return (
@@ -44,7 +34,7 @@ class App extends Component {
         </Row>
         <Row>
           <Col xs={12} md={6} >
-            <LocationList countries={countries} onSelectedLocation={this.handleSelectedLocation} />
+            <LocationListContainer countries={countries} />
           </Col>
           <Col xs={12} md={6} >
             <Paper elevation={6}>
@@ -66,32 +56,5 @@ class App extends Component {
   }
 
 }
-// Validaciones para que nuestro componente sea consistente:
-App.propTypes = {
-  setCity: PropTypes.func.isRequired,
-}
 
-/*
-Componente clase vs Componente Funcion
-Usamos Componente class cuando:
-Cuando necesitamos usar una de las instancias de ciclo de vida de REACT*/
-
-
-//Recibe un dispatch que a su vez retorna un objeto 
-//El objeto tendra las funciones que vamos a invocar para crear las acciones
-const mapDispatchToPropsActions = (dispatch) => {
-  return ({
-    //esta funcion recibe un parametro, invocamos un dispatch y recibe un action creator
-    setCity: value => dispatch(setCityActionCreator(value))
-  });
- }
-//Connect es una funcion que espera dos funciones
-// y asu vez devuelve otra funcion que espera le pasen un componente!!!
-// Ahora el componente que vamos a exportar no es el App solamente
-//export default App;
-// Va a ser el componente con la habilidad de conectarse con el store
-// const Appconnected = connect(null, mapDispatchToPropsActions)(App);
-// Eliminamos //export default App; y exportamos el nuevo componente...
-// export default Appconnected;
-// Podemos simplificar aun mas una vez ya entendido el concepto
-export default connect(null, mapDispatchToPropsActions)(App);
+export default App;
---
+++ clima-app/src/Containers/LocationListContainer.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import LocationList from '../Component/LocationList';
+import { connect } from 'react-redux';
+import { setCityActionCreator } from './../Actions';
+
+class LocationListContainer extends Component {
+
+    handleSelectedLocation = (city) => {
+        console.log(`handleSelectedLocation ${city}`)
+        //en vez de enviar directamente una action, se envia un actionCreator que devuelve 
+        this.props.setCity(city);
+    }
+
+    render() {
+        return (
+            <div>
+                <LocationList countries={this.props.countries} onSelectedLocation={this.handleSelectedLocation} />
+            </div>
+        );
+    }
+}
+
+LocationListContainer.propTypes = {
+    countries: PropTypes.array.isRequired,
+    setCity: PropTypes.func.isRequired,
+};
+
+/*
+Componente clase vs Componente Funcion
+Usamos Componente class cuando:
+Cuando necesitamos usar una de las instancias de ciclo de vida de REACT*/
+
+
+//Recibe un dispatch que a su vez retorna un objeto 
+//El objeto tendra las funciones que vamos a invocar para crear las acciones
+const mapDispatchToPropsActions = (dispatch) => {
+    return ({
+        //esta funcion recibe un parametro, invocamos un dispatch y recibe un action creator
+        setCity: value => dispatch(setCityActionCreator(value))
+    });
+}
+//Connect es una funcion que espera dos funciones
+// y asu vez devuelve otra funcion que espera le pasen un componente!!!
+// Ahora el componente que vamos a exportar no es el App solamente
+//export default App;
+// Va a ser el componente con la habilidad de conectarse con el store
+// const Appconnected = connect(null, mapDispatchToPropsActions)(App);
+// Eliminamos //export default App; y exportamos el nuevo componente...
+// export default Appconnected;
+// Podemos simplificar aun mas una vez ya entendido el concepto
+export default connect(null, mapDispatchToPropsActions)(LocationListContainer);
+
