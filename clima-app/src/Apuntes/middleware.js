31af077d98519aec595cb38d7a988457ef060937
Buhobit <byron.lopez.begins@gmail.com>
2018-11-25 00:10:23 -0500

    130. Redux-Thunk desde adentro

---
+++ clima-app/src/Apuntes/middleware.js
@@ -0,0 +1,28 @@
+const actionNormal  = payload => ({type: 'myAction', payload})
+
+//Action para mi dispatch
+//retorna una funcion dispatch
+const actionDelay = payload => {
+  return dispatch => {
+    dispatch(`inicio: ${payload}`);
+    window.setTimeout(() => dispatch(`terminó: ${payload}`),1000);
+  }
+}
+
+const myDispatch = texto => {
+  console.log(texto);
+}
+
+//Mientras tanto en el middleware
+// return ({ dispatch, getState}) => next => action => {
+
+const action = actionNormal("fetching");
+
+if(typeof action === 'function'){
+  console.log("action delay");
+  action(myDispatch);
+}else {
+  console.log("action normal");
+  console.log(action);
+}
+
