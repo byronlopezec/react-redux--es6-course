02e05fa001a5ba3129bbd11f169f41289559de2e
Buhobit <byron.lopez.begins@gmail.com>
2018-11-24 20:41:03 -0500

    123. Creación de Reducer

--- clima-app/src/Store/index.js
+++ clima-app/src/Store/index.js
@@ -1,5 +1,11 @@
 import { createStore } from 'redux';
+import { city } from '../Reducers/city';
+
+const initialState = {
+    city: 'Quito,ec'
+};
 
 // Para usar la extension Redux Devtools Extension en chrome se usa:
 // window.__REDUX_DEVTOOLS_EXTENSION && window.__REDUX_DEVTOOLS_EXTENSION()
-export const store = createStore(() => { }, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+export const store = createStore(city, initialState,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
