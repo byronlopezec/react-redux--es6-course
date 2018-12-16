55672af3024585128b0806319873345f2b08672f
Buhobit <byron.lopez.begins@gmail.com>
2018-12-16 13:09:10 -0500

    160. CustomersList

---
+++ customers-app/src/components/CustomerList.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import CustomerListItem from './CustomerListItem';
+
+const CustomerList = ({ customers, urlPath }) => {
+	return (
+		<div>
+			<div className='customers-list'>
+				{customers.map(c => {
+					<CustomerListItem key={c.dni} name={c.name} editAction={'Editar'} delAction={'Eliminar'} urlPath={urlPath} />;
+				})}
+			</div>
+		</div>
+	);
+};
+
+CustomerList.propTypes = {
+	customers: PropTypes.array.isRequired,
+	urlPath: PropTypes.string.isRequired
+};
+
+export default CustomerList;
