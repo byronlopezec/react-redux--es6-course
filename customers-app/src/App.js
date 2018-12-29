769aa2da85e5387ca894c75fb19c1cc602bf5bc5
Buhobit <byron.lopez.begins@gmail.com>
2018-12-29 12:42:12 -0500

    164. Utilización de Switch

--- customers-app/src/App.js
+++ customers-app/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 
 class App extends Component {
 	renderHome = () => <h1>Home</h1>;
 	renderCustomerListContainer = () => <h1>Customer List Container</h1>;
 	renderCustomerContainer = () => <h1>Customers Container</h1>;
-	renderCutomerNewContainer = () => <h1>Customer New Container</h1>;
+	renderCustomerNewContainer = () => <h1>Customer New Container</h1>;
 
 	// <Link to="/Customer">Cliente</Link> <br />
 	// <Link to="/Customer/3000">Cliente 3.000</Link>
@@ -15,10 +15,17 @@ class App extends Component {
 		return (
 			<Router>
 				<div className="App">
+					{/* exact permite indicar que el componente se muestre unicamente si la ruta es exacta */}
 					<Route exact path="/" component={this.renderHome} />
+					{/* lo que signifia que si yo pongo /customers se mostrara solamente el componente customersList */}
+					{/* si le quito el exact a "/customers" se mostrara tanto el componente renderHome("/")  */}
+					{/* y tambien el componente renderCustomerListContainer("/customers") */}
 					<Route exact path="/customers" component={this.renderCustomerListContainer} />
-					<Route exact path="/customers/:dni" component={this.renderCustomerContainer} />
-					<Route exact path="/cutomers/new" component={this.renderCutomerNewContainer} />
+					{/* Switch toma la primera coicidencia valida y descarta el resto! */}
+					<Switch>
+						<Route path="/customers/new" component={this.renderCustomerNewContainer} />
+						<Route path="/customers/:dni" component={this.renderCustomerContainer} />
+					</Switch>
 				</div>
 			</Router>
 		);
